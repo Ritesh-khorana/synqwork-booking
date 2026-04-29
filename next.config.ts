048a3f28d56d4feabb61f0ae18aac04772cea40a
Ritesh-khorana <ritesh@synq.work>
2026-04-29 12:28:45 +0530

    Update next.config.ts
    added synqwork

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "synqwork.com" },
+      { protocol: "https", hostname: "www.synqwork.com" },
+    ],
+  },
 };
 
 export default nextConfig;
